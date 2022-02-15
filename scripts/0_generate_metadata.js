545a3164feab5659de59aef78bb61a64054bb87f
yahyaoukharta <youkharta@gmail.com>
2022-02-15 05:54:36 +0100

    ff

---
+++ scripts/0_generate_metadata.js
@@ -0,0 +1,64 @@
+
+let token = { 
+    "id": "1",
+    "name": "Paraporo #1",
+    "image": "ipfs://QmRZwhrjnRe4tG1vEibber73qZXoeTxuk5CTzq7YeJW32k/unrevealed.mp4",
+    "description": "Leave the drab reality and enter the world of Paraporo. Paraporo is home to 5,555 generative arts where colors reign supreme.",
+    "attributes": [
+      { 
+        "trait_type": "Status",
+        "value": "Unrevealed"
+      }
+    ]
+  }
+
+let tokenTraits = 
+    {
+        "id": "1",
+        "name": "Kuyaku #1",
+        "image": "ipfs://QmZfjpjxLw3oE28Ac41SuFnytKVJpkV8Y6ZNq5m3JB9jzN/1.jpg",
+        "description": "Karafuru is home to 5,555 generative arts where colors reign supreme. Leave the drab reality and enter the world of Karafuru by Museum of Toys.",
+        "attributes": [
+          {
+            "trait_type": "Background",
+            "value": "Orange"
+          },
+          {
+            "trait_type": "Skin",
+            "value": "Black"
+          },
+          {
+            "trait_type": "Outfit",
+            "value": "School Outfit"
+          },
+          {
+            "trait_type": "Eyes",
+            "value": "Bored"
+          },
+          {
+            "trait_type": "Headgear",
+            "value": "Fire Mohawk"
+          },
+          {
+            "trait_type": "Mouth",
+            "value": "Onigiri"
+          },
+          {
+            "trait_type": "Companion",
+            "value": ""
+          },
+          {
+            "trait_type": "Base",
+            "value": "Kuyaku"
+          },
+          {
+            "trait_type": "Legendary",
+            "value": ""
+          }
+        ]
+      }
+
+const dotenv = require("dotenv")
+  dotenv.config()
+
+let 
